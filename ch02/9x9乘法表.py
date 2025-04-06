26f233bfe16d5d64776b999829675402bf521339
LAPTOP-OKCC8NI6\Wei-Yang Chou <sofia960208@gmail.com>
2025-04-06 11:31:32 +0800

    ch02

---
+++ ch02/9x9乘法表.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 20:21:55 2023
+
+@author: Wei-Yang Chou
+"""
+
+
+for x in range(1,10):
+    for y in range(1,10):
+        print(f"{x}X{y}={x*y}",end=' ')
+
+    
+    
+    
